e810ce67b19c38be305726c3a864b7b358015e78
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-11-14 14:33:11 +0000

    ascii code display

---
+++ displaying an ascii code.py
@@ -0,0 +1,9 @@
+#Lewis Travers
+#14/11/2014
+#displaying the ascii code for a given character
+
+character = input("Please enter a character: ")
+
+code = ord(character)
+
+print("The ascii code for the character is {0}.".format(code))
