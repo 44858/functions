3d0316e5e8f2fc1a9aa1ff47bc35a3d08c5d0595
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-11-18 13:02:49 +0000

    displaying a character for a given ascii code

---
+++ displaying the character for a given ascii code.py
@@ -0,0 +1,9 @@
+#Lewis Travers
+#14/11/2014
+#displaying the character for a given ascii code
+
+code = int(input("Please enter an ascii code: "))
+
+character = chr(code)
+
+print("The character equivalent of the code entered is {0}.".format(character))
