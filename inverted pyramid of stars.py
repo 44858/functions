32cd13f35abcb559ab15ff0e4edcf244b9164a70
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-11-28 14:30:38 +0000

    inverted pyramid

---
+++ inverted pyramid of stars.py
@@ -0,0 +1,12 @@
+#Lewis Travers
+#28/11/2014
+#inverted pyramid of stars
+
+def number_input():
+    number = int(input("Please enter an odd number: "))
+    return number
+
+def number_validation(number):
+    if number % 2 == 0:
+        for count in range:
+        
