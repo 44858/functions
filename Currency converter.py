eb93a90440a4ab50ca05b31eea14ff2474cdc01d
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-12-05 14:07:54 +0000

    currency converter (finish at home)

---
+++ Currency converter.py
@@ -0,0 +1,13 @@
+#Lewis Travers
+#05/12/2014
+#Currency converter
+
+def currency_input():
+    print("Currency converter using Pounds, Dollars and Euros")
+    currency_one = input("Please enter the currency you would like converted from: "))
+    currency_two = input("Please enter the currency you would like the first to be converted to: "))
+    amount = int(input("Please enter the amount of currency that you would like converted: "))
+    return currency_one, currency_two, amount
+
+def convert_currency(currency_one, currency_two, amount):
+    if 
