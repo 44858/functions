3b11434be1c69072ca18ec210a7b011ee528fee8
l.travers55@gmail.com <44858@PC004142.coll.lr.local>
2014-12-09 13:02:18 +0000

    storing names

---
+++ storing students names.py
@@ -0,0 +1,16 @@
+#Lewis Travers
+#09/12/2014
+#storing and editing students names
+
+def getting_eight_names():
+    first_name = input("Please enter the name of the first student: ")
+    second_name = input("Please enter the name of the second student: ")
+    third_name = input("Please enter the name of the third student: ")
+    fourth_name = input("Please enter the name of the fourth student: ")
+    fifth_name = input("Please enter the name of the fifth student: ")
+    sixth_name = input("Please enter the name of the sixth student: ")
+    seventh_name = input("Please enter the name of the seventh student: ")
+    eighth_name = input("Please enter the name of the eighth student: ")
+    return first_name, second_name, third_name, fourth_name, fifth_name, sixth_name, seventh_name, eighth_name
+
+def
