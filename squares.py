46ce164b7e8774545dc773589ff4de882fd71b52
44858 <l.travers55@gmail.com>
2014-12-11 21:03:50 +0000

    squares

---
+++ squares.py
@@ -0,0 +1,8 @@
+def squares():
+    for count in range(1, 51):
+        square = count * count
+        print("The square of {0} is {1}".format(count, square))
+              
+square_number = squares()
+print(square_number)
+
